435fde38ce68efa2e5267f90cc0ef5fd76f066df
Aleszp <aleksander.szsz@gmail.com>
2017-02-14 17:45:13 +0100

    Poprawiono algorytm zaokrąglania.

--- other.c
+++ other.c
@@ -10,6 +10,6 @@ void delay40(uint8_t times)
 
 uint16_t round16(uint16_t src,uint8_t bits)
 {
-	src+=(1<<bits);
-	return (src>>bits);
+	src+=(1<<(bits-1));						//zwiększ część pomijalną o 1/2, w ten sposób 
+	return (src>>bits);						//obcinając ją, zaokrąglamy do bliższej wartości, nie w dół
 }
